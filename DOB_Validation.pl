2c625665fe142bac681e0b0ec99ca7940e77094c
Naruto123Jiraiya <115707956+Naruto123Jiraiya@users.noreply.github.com>
2022-11-10 20:41:27 +0530

    Add files via upload

---
+++ DOB_Validation.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use 5.010;
+#use strict;
+use warnings;
+
+#print "Hello World!\n";
+print"enter the dob:";
+chomp($dob=<>);
+if($dob=~ m/(0[1-9]|1[0-9]|2[0-9]|[30|31])\-(0[1-9]|1[0-2])\-([1990..2021])$/){
+	print"date of birth is valid:$dob\n";
+}
+else{
+	print"date of birth is not  valid:$dob\n";
+}
